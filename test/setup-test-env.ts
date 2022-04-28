b954ef6142f53753ad476b0c76c7ca12f19be3f3
Karl Jensen <karljensen@gmail.com>
2022-04-28 14:38:03 -0700

    Add custom Matcher for thown responses

--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -9,3 +9,29 @@ declare global {
 }
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
+expect.extend({
+  async toThrowResponse(received, status) {
+    try {
+      await received();
+
+      return {
+        message: () => `expected response to be thrown`,
+        pass: false,
+      };
+    } catch (response: any) {
+      if (response.status !== status) {
+        return {
+          message: () =>
+            `expected response to be thrown with status ${status} received ${response.status}`,
+          pass: false,
+        };
+      }
+    }
+
+    return {
+      message: () => "Expected response not to be thrown",
+      pass: true,
+    };
+  },
+});

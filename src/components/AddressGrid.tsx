015ace1b1ac2ce17c643368ee297f7be8ce4c782
Karl Jensen <karljensen@gmail.com>
2022-05-05 15:41:16 -0700

    Basic support for smaller screens

--- src/components/AddressGrid.tsx
+++ src/components/AddressGrid.tsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from "@remix-run/react";
 import classNames from "classnames";
+import { useState, useEffect } from "react";
 import StatusTag from "./StatusTag";
 
 const AddressColumn = (props) => {
@@ -56,8 +57,32 @@ const chunk = (list: unknown[], size: number) => {
   return chunks;
 };
 
+type PixelWidth = "720px" | "960px" | "1200px";
+
+const lookup: Record<PixelWidth, number> = {
+  "1200px": 25,
+  "960px": 34,
+  "720px": 50,
+};
+
+const getWidthBasedSize = () => {
+  const match: PixelWidth | undefined = (
+    Object.keys(lookup) as (keyof typeof lookup)[]
+  ).find((key) => window.matchMedia(`(min-width: ${key})`).matches);
+
+  if (match) {
+    return lookup[match];
+  }
+
+  return 100;
+};
+
 export default function AddressGrid(props) {
-  const columns = chunk(props.addresses, 25);
+  const [columns, setColumns] = useState<Array<any[]>>([]);
+
+  useEffect(() => {
+    setColumns(chunk(props.addresses, getWidthBasedSize()));
+  }, [props.addresses]);
 
   return (
     <article className="address-grid">
